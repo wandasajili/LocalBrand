17300d0896ee920bfd1c769e023dea25fc5a61d2
wandasajili22 <sajili.wanda@gmail.com>
2024-12-31 06:35:24 +0700

    update txt

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -121,7 +121,7 @@
                             <img class="size-10 rounded-full"
                                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                 alt="">
-                        </div>
+                        </div> 
                         <div class="ml-3">
                             <div class="text-base/5 font-medium text-white">Tom Cook</div>
                             <div class="text-sm font-medium text-gray-400">tom@example.com</div>
