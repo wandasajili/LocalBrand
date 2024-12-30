bc2725e04dc7e2e5749e30892e9b08a9e3a4691e
wandasajili22 <sajili.wanda@gmail.com>
2024-12-31 06:48:30 +0700

    update txt

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -229,7 +229,7 @@
                         </ul>
                     </div>
                     <div>
-                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2>
+                        {{-- <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2> --}}
                         {{-- <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4">
                                 <a href="#" class="hover:underline">iOS</a>
