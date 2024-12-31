b20d838d70cef87bb816b9e3a72b3215ab04f38e
wandasajili22 <sajili.wanda@gmail.com>
2024-12-31 07:34:08 +0700

    update txt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -251,7 +251,6 @@
                             class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">2</a>
                         <a href="#"
                             class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">3</a>
-
                         <a href="#"
                             class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
                             <span class="sr-only">Next</span>
