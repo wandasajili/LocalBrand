b431fc781326cf800c24089563b22ec837278ca9
wandasajili22 <sajili.wanda@gmail.com>
2025-01-01 15:25:00 +0700

    update txt

--- resources/views/sale.blade.php
+++ resources/views/sale.blade.php
@@ -17,7 +17,6 @@
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
-
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <a href="/"
