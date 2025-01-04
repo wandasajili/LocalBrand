5138bbc29a5b34ff8f395ee092731c38079ef243
wandasajili22 <sajili.wanda@gmail.com>
2025-01-04 20:02:40 +0700

    update txt

--- resources/views/Profile.blade.php
+++ resources/views/Profile.blade.php
@@ -25,8 +25,8 @@
                                 <img src="https://randomuser.me/api/portraits/men/94.jpg" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
 
                                 </img>
-                                <h1 class="text-xl font-bold">John Doe</h1>
-                                <p class="text-gray-700">Software Developer</p>
+                                <h1 class="text-xl font-bold">Wanda Sajili</h1>
+                                <p class="text-gray-700">Admin Toko TMS</p>
                                 <div class="mt-6 flex flex-wrap gap-4 justify-center">
                                     <a href="#" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Contact</a>
                                     <a href="#" class="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded">Resume</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,8 @@
                         <!-- Category Card 1 -->
                         <div class="bg-white rounded shadow p-4 text-center">
                             <div class="relative">
-                                <span class="absolute top-2 left-2 bg-black text-white text-xs font-bold px-2 py-1 rounded">Sale</span>
+                                <span
+                                    class="absolute top-2 left-2 bg-black text-white text-xs font-bold px-2 py-1 rounded">Sale</span>
                                 <img src="https://hgbasiclabs.com/cdn/shop/products/6d9ef2f2-c83a-4f67-868d-75164b3dd11c-GREY-WHITE_e8d2f675-9b65-4c38-ba34-13de8e5617cf.jpg?v=1710819445&width=600"
                                     alt="Produk 3" class="w-full h-auto rounded mb-4">
                             </div>
@@ -213,41 +214,6 @@
                     </div>
                 </div>
             </section>
-
-            <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-
-                <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                    <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                        <a href="#"
-                            class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                            <span class="sr-only">Previous</span>
-                            <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
-                                data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                        <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
-                        <a href="#/Catolog" aria-current="page"
-                            class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
-                        <a href="#"
-                            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">2</a>
-                        <a href="#"
-                            class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">3</a>
-                        <a href="#"
-                            class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                            <span class="sr-only">Next</span>
-                            <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
-                                data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    </nav>
-                </div>
-            </div>
         </main>
 
         <footer class="bg-white dark:bg-gray-900">
